0b498220f2f56fe602f93008d53ce6df93519b1f
Izaan Ahmed Khan <izaanahmedkhan@gmail.com>
2022-12-17 20:38:55 +0500

    .

--- day_1/src/components/NavBar.js
+++ day_1/src/components/NavBar.js
@@ -3,10 +3,16 @@ import React from 'react'
 export const NavBar = () => {
   return (
     <>
-    <nav className='m-5'>
+    <nav className='flex m-5'>
         <div> 
           <img className='w-48' src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Valorant_pink_version_logo.svg/2560px-Valorant_pink_version_logo.svg.png'/>
         </div>
+        {/* <div>
+          <ul className='text-lg flex list-none'>
+            <a className='mx-5' href='/'> <li> Home </li> </a>
+            <a href='/'> <li> About </li> </a>
+          </ul>
+        </div> */}
     </nav>
     </>
   )
