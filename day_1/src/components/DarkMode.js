f64ecab7994d0ddaf411c3fe5659a9ab29615469
Izaan Ahmed Khan <izaanahmedkhan@gmail.com>
2022-12-17 20:14:47 +0500

    toggle mode

---
+++ day_1/src/components/DarkMode.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react'
+
+export const DarkMode = () => {
+
+    const [theme, setTheme] = useState("light-theme")
+    
+    const toggleTheme = () => {
+        (theme === "dark-theme ") ? setTheme("light-theme") : setTheme("dark-theme")
+    }
+
+    useEffect(() => {
+            document.body.className = theme;
+            console.log('oo')
+        }, [theme]);
+
+    return (
+    <>
+        <main>
+            <section>
+                <div className='flex'>
+                    <div className='text-xl w-1/2 my-40 mx-10'>
+                        <p>Neon is a young Filipina woman with olive skin and a short stature. She sports an athletic attire with black and blue running leggings along with a pair 
+                            of sneakers. She also wears a dark-blue cropped vest that appears to act as electrical conduits for her powers. Her arms and face have light blue patterns 
+                            that can illuminate with her radiant abilities. She has shoulder-length shaggy hair, colored mainly electric blue with streaks of yellow throughout and 
+                            wears it in a half-up pigtails hairstyle. She also wears a belt and accessorizes with a golden bracelet on each arm. <br/>
+                            When using her radiant powers to channel electricity through her body, the patterns on her arms, face, as well as her eyes glow light blue along with 
+                            the her hair which stands-up due to the electricity.</p>
+                        <button className='btn border rounded-md p-2 my-5 text-sm' onClick={() => toggleTheme()}> Toggle Mode </button>
+                    </div>
+                    <div>
+                        <img className='w-full' src='https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png'/>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </>
+  )
+}
---
+++ day_1/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <>
+    <nav className='m-5'>
+        <div> 
+          <img className='w-48' src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Valorant_pink_version_logo.svg/2560px-Valorant_pink_version_logo.svg.png'/>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default NavBar;
