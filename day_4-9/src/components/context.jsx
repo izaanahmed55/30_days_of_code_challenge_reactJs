73afe1e4af1b56d6b4fa82e052979661e756789f
Izaan Ahmed Khan <izaanahmedkhan@gmail.com>
2022-12-22 02:41:46 +0500

    fetchAPI

--- day_4-9/src/components/context.jsx
+++ day_4-9/src/components/context.jsx
@@ -28,3 +28,14 @@ const useGlobalContext = () => {
 }
 
 export {AppContext, AppProvider, useGlobalContext};
+
+
+
+// Here is your key: 8d8cd1b9
+
+// Please append it to all of your API requests,
+
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=8d8cd1b9
+
+// Click the following URL to activate your key: http://www.omdbapi.com/apikey.aspx?VERIFYKEY=44d0b33c-9476-4c6b-9f7e-77d7c2c6a2b7
+// If you did not make this request, please disregard this email.
